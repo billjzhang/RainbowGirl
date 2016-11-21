8e3e7ac122ab824cc2f3055a59cc9853f8ec5c96
billjzhang <bill223@qq.com>
2016-11-21 15:41:21 +0800

    test

---
+++ Assets/RainbowGirl/Script/Template/Singleton.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Singleton<T> where T : new()
+{
+    static private T sInstance;
+    static public T GetSingleton()
+    {
+        if(sInstance == null)
+        {
+            sInstance = new T();
+        }
+
+        return sInstance;
+    }
+
+}
